4b0fdc58192237d488c49962c08e268614e40020
Sergii Brodeskur <romedal7@gmail.com>
2018-02-24 19:21:14 +0200

    repaierd build

--- Inc/main.h
+++ Inc/main.h
@@ -49,6 +49,8 @@
 
 /* Private define ------------------------------------------------------------*/
 
+#define Button_Pin GPIO_PIN_0
+#define Button_GPIO_Port GPIOA
 #define LD4_Pin GPIO_PIN_8
 #define LD4_GPIO_Port GPIOC
 #define LD3_Pin GPIO_PIN_9
